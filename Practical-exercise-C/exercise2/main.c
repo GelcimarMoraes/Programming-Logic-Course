7d578c077d4aa1a5c7e9e502cc9148fbda427a63
GelcimarMoraes <60938131+GelcimarMoraes@users.noreply.github.com>
2023-11-21 15:27:33 -0300

    exercise

---
+++ Practical-exercise-C/exercise2/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ()
+{
+    int ano_nascimento = 1990;
+    int ano_atual = 2023;
+
+    int resultado = (ano_atual - ano_nascimento);
+
+    printf("O Gelcimar tem %d anos",resultado);
+
+    return 0;
+}
